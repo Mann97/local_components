8f3ce77efaad129f6544d493f2dd6c54911bf2ee
Manthan Patel <manthan.patel@kodytechnolab.com>
2024-10-24 11:08:59 +0530

    initial commit

---
+++ lib/main_component.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainComponent extends StatefulWidget {
+  const MainComponent({super.key});
+
+  @override
+  State<MainComponent> createState() => _MainComponentState();
+}
+
+class _MainComponentState extends State<MainComponent> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
