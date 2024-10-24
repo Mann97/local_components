628f18ddd2c73549ca09bba2c018e816f526db96
Manthan Patel <manthan.patel@kodytechnolab.com>
2024-10-24 12:41:39 +0530

    code updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,25 @@
-// export 'main_component.dart';
+export 'main_component.dart';
 
-import 'package:flutter/material.dart';
-import 'package:local_components/main_component.dart';
+// import 'package:flutter/material.dart';
+// import 'package:local_components/main_component.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Local Components',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const SizedBox(),
-    );
-  }
-}
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Local Components',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const SizedBox(),
+//     );
+//   }
+// }
--- lib/main_component.dart
+++ lib/main_component.dart
@@ -1,2 +1,7 @@
 export 'utils/widgets/common_button.dart';
 export 'utils/widgets/common_svg.dart';
+export 'utils/widgets/common_dialogs.dart';
+export 'utils/widgets/common_permission_widget.dart';
+export 'utils/widgets/common_text.dart';
+export 'utils/helper/image_cropper_dialog.dart';
+export 'utils/helper/image_picker_manager.dart';
